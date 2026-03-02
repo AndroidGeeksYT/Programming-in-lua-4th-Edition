8e3fedeb4279341b0e8108127569b0ad9d2a7a2a
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-03-03 07:43:25 +0800

    feat(chunk): add a simple chunk example

---
+++ chunk/chunk.lua
@@ -0,0 +1,10 @@
+-- simply put lua wrap the code above as a nameless function
+-- function (...)
+--   local chunk = 1 + 2
+--   print("The chunk says 1 + 2 is: " .. chunk)
+-- return chunk
+-- end
+
+local chunk = 1 + 2
+print("The chunk says 1 + 2 is: " .. chunk)
+return chunk
