6c13ae4ccdffad74814371cf63d3670b22bc8644
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-03-04 09:28:01 +0800

    feat(lexical): add some lexical conventions

---
+++ lexical/lexical.lua
@@ -0,0 +1,17 @@
+-- some lexical convention
+a = 1
+b = a * 2
+print(b)
+
+-- another example with semicolon
+c = 1;
+d = c * 2;
+print(d)
+
+-- semicolon with one statement
+e = 1; f = e * 2;
+print(f)
+
+-- without semicolon
+g = 1 j = g * 2
+print(j)
