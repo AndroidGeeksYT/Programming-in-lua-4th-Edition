8531d7ae34d2c796deb812a8aa14c9d389886f2e
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-03-11 00:59:42 +0800

    feat(comments): add an example of comments in lua

---
+++ comments/comments.lua
@@ -0,0 +1,20 @@
+-- This is a single line comment
+
+--[[ This
+--  is
+--  a
+--  multi
+--  line
+--  long
+--  comment]]
+
+--[[
+-- A trick comment
+    print(10)
+--]]
+
+-- A comment that can reactivate a block of code by adding a single
+-- hyphen to the first line of comment.
+---[[
+print(10)
+--]]
